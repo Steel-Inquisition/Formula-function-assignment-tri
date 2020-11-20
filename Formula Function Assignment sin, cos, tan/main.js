ad20b26229645cc68b739b73e8f5b5f5932fd056
Steel-Inquisition <62898596+Steel-Inquisition@users.noreply.github.com>
2020-11-19 20:44:05 -0700

    Add files via upload

--- Formula Function Assignment sin, cos, tan/main.js
+++ Formula Function Assignment sin, cos, tan/main.js
@@ -5,31 +5,46 @@ let hypoEl = document.getElementById("hypo");
 let adjEl = document.getElementById("adj");
 let oppEl = document.getElementById("opp");
 let calcEl = document.getElementById('calc');
+let typeEl = document.getElementById('type');
 let outputEl = document.getElementById('output');
 
 // Event Listener
-calcEl.addEventListener("select", calcNumber);
+calcEl.addEventListener("click", calcNumber);
 
 // Event Function
 function calcNumber() {
     let hypo = hypoEl.value;
     let adj = adjEl.value;
     let opp = oppEl.value;
-    let calc = calcEl.value;
+    let calc = typeEl.value;
+
+    let n;
 
     // Process and Output
     outputEl.innerHTML = lengthGet(hypo, adj, opp, calc);
 }
 
-function lengthGet(hypo, adj, opp, calc, sin, cos, tan) {
+function lengthGet(hypo, adj, opp, calc) {
 
-    // if (calc == sin) {
-    //     let n = (opp / hypo);
-    // }
+    if (calc == 'SIN') {
+        n = (opp / hypo);
 
-    // n = Math.asin(n) * 180 / Math.PI;
+        console.log("sin");
+    } else if (calc == 'COS') {
+        n = (adj / hypo);
 
-    console.log(calc);
+        console.log("cos");
+    } else if (calc == 'TAN') {
+        n = (opp / adj);
 
-    // return n;
+        console.log("tan");
+    }
+
+    n = Math.asin(n) * 180 / Math.PI;
+
+    if (hypo < opp || hypo < adj) {
+        n = "The hypotenuse cannot be smaller than the other lengths."
+    }
+
+    return n;
 }
